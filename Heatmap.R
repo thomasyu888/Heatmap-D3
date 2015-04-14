f3d6cc1757832dd74bec4918ad18a483d985fa8e
Thomas Yu <thomasyu888@gmail.com>
2015-04-14 14:53:59 -0700

    Update Heatmap.R

--- Heatmap.R
+++ Heatmap.R
@@ -1,6 +1,6 @@
 
-genes <- read.csv(file="INPUT FILE NAME",head=TRUE,row.names=1)
-metadatas <- read.csv(file="metadata.csv",head=TRUE,row.names=1)
+genes <- read.csv(file="Example.csv",head=TRUE,row.names=1)
+metadatas <- read.csv(file="metaExample.csv",head=TRUE,row.names=1)
 data <- list(main = genes,metadatas = metadatas)
 
 runApp(

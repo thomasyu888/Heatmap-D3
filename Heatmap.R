65492b8b2e7b16d818d0033b32395dc4cc8c5ee2
Thomas Yu <thomasyu888@gmail.com>
2015-04-24 15:33:25 -0700

    Update Heatmap.R

--- Heatmap.R
+++ Heatmap.R
@@ -24,8 +24,8 @@ HCtoJSON<-function(hc){
   return(JSON)
 }
 
-genes <- read.csv(file="PCBC_geneExpr_data.csv",head=TRUE,row.names=1)
-metadatas <- read.csv(file="metadata.csv",head=TRUE,row.names=1)
+genes <- read.csv(file="example.csv",head=TRUE,row.names=1)
+metadatas <- read.csv(file="metaExample.csv",head=TRUE,row.names=1)
 genes <- as.matrix(genes)
 rng <- range(genes)
 

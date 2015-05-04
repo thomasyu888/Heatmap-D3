d323a0bbf85c23745b0b9859180ca7a2d901b2d1
Thomas Yu <thomasyu888@gmail.com>
2015-05-04 00:14:45 -0700

    Create server.R

---
+++ server.R
@@ -0,0 +1,18 @@
+require(rCharts)
+require(shiny)
+
+
+m <- read.csv("PCBC_geneExpr_data.csv",row.names = 1, header = TRUE)
+d <- read.csv("metadata.csv",row.names=1, header=TRUE)
+m <-as.matrix(m)
+d <- as.matrix(d)
+f <- runif(69)
+f <- matrix(f,69,1)
+
+shinyServer(function(input, output) {
+  output$myChart <- renderChart2({
+    source("Heatmap.R")
+    p1 <- iHeatmap(m,d,f, input$x)
+    return(p1)
+  })
+})

915f559ba0a2dac5c2da2d218f2e4315736793ae
Thomas Yu <thomasyu888@gmail.com>
2015-05-04 14:45:29 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,17 +1,30 @@
 require(rCharts)
-require(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Interactive Heatmap"),
+  
+  sidebarPanel(
+    selectInput(inputId = "x",
+                label = "Clustering method",
+                choices = c('complete', 'average','ward.D','ward.D2','single','mcquitty','median','centroid'),
+                selected = "complete"),
+    
+    selectInput(inputId = "y",
+                label = "Cluster rows",
+                choices = c('TRUE', 'FALSE'),
+                selected = 'TRUE'),
 
-m  <- runif(1000)
-m <- matrix(m, ncol=10)
-d <- runif(10)
-d <- matrix(d,10,1)
-f <- runif(100)
-f <- matrix(f,100,1)
+    selectInput(inputId = "z",
+                label = "Cluster columns",
+                choices = c('TRUE','FALSE'),
+                selected = 'TRUE'),
+    
+    selectInput(inputId = "v",
+                label = "Distance method",
+                choices = c('euclidean','maximum','manhattan','canberra','binary','minkowski'),
+                selected = 'euclidean')
+  ),
 
-shinyServer(function(input, output) {
-  output$myChart <- renderChart2({
-    source("Heatmap.R")
-    p1 <- iHeatmap(m,d,f, input$x)
-    return(p1)
-  })
-})
+  mainPanel(
+    showOutput("myChart","libraries/heatmap")
+  )
+))

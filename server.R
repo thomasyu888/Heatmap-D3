9723db69f8f195f28f61171272b3c68f8fb3e536
Thomas Yu <thomasyu888@gmail.com>
2015-05-04 14:42:43 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,13 +1,12 @@
 require(rCharts)
 require(shiny)
+m  <- runif(1000)
+m <- matrix(m, ncol=10)
+d <- runif(10)
+d <- matrix(d,10,1)
+f <- runif(100)
+f <- matrix(f,100,1)
 
-
-m <- read.csv("PCBC_geneExpr_data.csv",row.names = 1, header = TRUE)
-d <- read.csv("metadata.csv",row.names=1, header=TRUE)
-m <-as.matrix(m)
-d <- as.matrix(d)
-f <- runif(69)
-f <- matrix(f,69,1)
 
 shinyServer(function(input, output) {
   output$myChart <- renderChart2({

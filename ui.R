337593888eecb8b49ff9b3bb8776afed7f58b2f0
Thomas Yu <thomasyu888@gmail.com>
2015-05-04 14:46:10 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -4,11 +4,26 @@ shinyUI(pageWithSidebar(
   
   sidebarPanel(
     selectInput(inputId = "x",
-                label = "Choose method",
+                label = "Clustering method",
                 choices = c('complete', 'average','ward.D','ward.D2','single','mcquitty','median','centroid'),
-                selected = "complete")
-   
+                selected = "complete"),
+    
+    selectInput(inputId = "y",
+                label = "Cluster rows",
+                choices = c('TRUE', 'FALSE'),
+                selected = 'TRUE'),
+
+    selectInput(inputId = "z",
+                label = "Cluster columns",
+                choices = c('TRUE','FALSE'),
+                selected = 'TRUE'),
+    
+    selectInput(inputId = "v",
+                label = "Distance method",
+                choices = c('euclidean','maximum','manhattan','canberra','binary','minkowski'),
+                selected = 'euclidean')
   ),
+
   mainPanel(
     showOutput("myChart","libraries/heatmap")
   )
